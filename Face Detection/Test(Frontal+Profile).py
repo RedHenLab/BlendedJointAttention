e7d18f99babbbd9f54ee25b344ef0458fea2876a
Soumitra <agarwalsoumitra1504@gmail.com>
2016-05-16 17:18:17 +0530

    Test code addition for profile faces

--- Face Detection/Test(Frontal+Profile).py
+++ Face Detection/Test(Frontal+Profile).py
@@ -4,18 +4,17 @@ import sys
 faceCascade1 = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_alt2.xml')
 faceCascade2 = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
 faceCascade3 = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_alt.xml')
-faceCascade4 = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_extended.xml')
 faceCascade5 = cv2.CascadeClassifier('haarcascades/haarcascade_profileface.xml')
 faceCascade6 = cv2.CascadeClassifier('lbp_cascades/lbpcascade_profileface.xml')
 
-frame = cv2.imread('/Test_Images/Test4.jpg')
+frame = cv2.imread('Test_Images/Test6.jpg')
 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
 faces1 = faceCascade1.detectMultiScale(gray, 1.1, 5)
 faces2 = faceCascade2.detectMultiScale(gray, 1.1, 5)
 faces3 = faceCascade3.detectMultiScale(gray, 1.1, 5)
-faces4 = faceCascade3.detectMultiScale(gray, 1.1, 5)
-faces5 = faceCascade3.detectMultiScale(gray, 1.1, 5)
+faces5 = faceCascade5.detectMultiScale(gray, 1.1, 5)
+faces6 = faceCascade6.detectMultiScale(gray, 1.1, 5)
 
 # Draw a rectangle around the faces
 for (x, y, w, h) in faces1:
@@ -30,13 +29,12 @@ for (x, y, w, h) in faces3:
     cv2.rectangle(frame, (x, y), (x+w, y+h), (127, 128, 0), 2)
     roi_gray = gray[y:y+h, x:x+w]
     roi_color = frame[y:y+h, x:x+w]
-for (x, y, w, h) in faces4:
-    cv2.rectangle(frame, (x, y), (x+w, y+h), (127, 128, 0), 2)
-    roi_gray = gray[y:y+h, x:x+w]
-    roi_color = frame[y:y+h, x:x+w]
 for (x, y, w, h) in faces5:
     cv2.rectangle(frame, (x, y), (x+w, y+h), (127, 128, 0), 2)
     roi_gray = gray[y:y+h, x:x+w]
     roi_color = frame[y:y+h, x:x+w]
-
-cv2.imwrite( "/Result_Images/Result4.jpg", frame);
+for (x, y, w, h) in faces6:
+    cv2.rectangle(frame, (x, y), (x+w, y+h), (127, 128, 0), 2)
+    roi_gray = gray[y:y+h, x:x+w]
+    roi_color = frame[y:y+h, x:x+w]
+cv2.imwrite('Result_Images/Result6.jpg', frame);
