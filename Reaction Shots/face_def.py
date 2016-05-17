395aafec1319b868f696891d0f04c1cfb3ddf2c1
Iceflame007 <debayan.das@students.iiit.ac.in>
2016-05-18 01:10:01 +0530

    Added basic class definition to represent a Face

--- Reaction Shots/face_def.py
+++ Reaction Shots/face_def.py
@@ -2,9 +2,17 @@
 
 class face(object):
 	"""A Class to represent a face"""
-
-	def __init__(self, args):
-		self.args = args
-		pass
-
-		
+	center = (0,0)
+	width = 0
+	height = 0
+	vector = [0]
+	
+	def __init__(self, center=(0,0), width=0, height=0, vector):
+		""" Input params are (x,y) coords of center of face's bounding box, width
+		    and height of box and the 128-dimensional vector representation of the face """ 
+		super(face, self).__init__()
+		self.center = center
+		self.width = width
+		self.height = height
+		self.vector = vector
+	
