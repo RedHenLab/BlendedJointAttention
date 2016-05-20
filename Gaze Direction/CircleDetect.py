d3cba2f6b9ef0d8bffec31af0d2373bd9b4b43e0
Soumitra <agarwalsoumitra1504@gmail.com>
2016-05-21 01:01:11 +0530

    Parameter tuning update

--- Gaze Direction/CircleDetect.py
+++ Gaze Direction/CircleDetect.py
@@ -3,15 +3,14 @@ import sys
 import numpy as np
 
 frame = cv2.imread('Test_Images/Test1.jpg')
-gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+frame = cv2.medianBlur(frame,5)
 split = cv2.Canny(frame,100,200)
-circles = cv2.HoughCircles(split,cv2.HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=0,maxRadius=0)
+gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+circles = cv2.HoughCircles(split,cv2.HOUGH_GRADIENT,1.04,100)
 circles = np.uint16(np.around(circles))
 for i in circles[0,:]:
-	cv2.circle(split,(i[0],i[1]),i[2],(0,255,0),2)
-	cv2.circle(split,(i[0],i[1]),2,(0,0,255),3)
-	cv2.circle(frame,(i[0],i[1]),i[2],(0,255,0),2)
-	cv2.circle(frame,(i[0],i[1]),2,(0,0,255),3)
+	cv2.circle(gray,(i[0],i[1]),i[2],(0,255,0),2)
+	cv2.circle(gray,(i[0],i[1]),2,(0,0,255),3)
 	print(i)
-cv2.imwrite('Result_Images/Result3.jpg', split)
-cv2.imwrite('Result_Images/Result1.jpg', frame);
+cv2.imwrite('Result_Images/Result3.jpg', gray)
+cv2.imwrite('Result_Images/Result1.jpg', split);
