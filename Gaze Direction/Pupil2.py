98d98eff4b32cdb3bb78c6b0ce5ee9e3b0ba852c
root <agarwalsoumitra1504@gmail.com>
2016-05-20 16:37:50 +0530

    Code change.Full update

--- Gaze Direction/Pupil2.py
+++ Gaze Direction/Pupil2.py
@@ -1,35 +1,43 @@
 import cv2
-import numpy as np
-def get_cascades():
-	faceCascade1 = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_alt2.xml')
-	faceCascade2 = cv2.CascadeClassifier('haarcascades/haarcascade_profileface.xml')
-	eyecascade1 = cv2.CascadeClassifier('haarcascades/haarcascade_eye.xml')
-	eyecascade2 = cv2.CascadeClassifier('haarcascades/haarcascade_eye_tree_eyeglasses.xml')
-def video_capture():
-	cam = cv2.VideoCapture(-1)
-	cam.set(3,640)
-	cam.set(4,480)
-	video_capture = cam
-	return video_capture
-#define main function
-def pupil_detect():
-	get_cascades()
-	video_capture = video_capture()
-	cam = cv2.VideoCapture(-1)
-	cam.set(3,640)
-	cam.set(4,480)
-	video_capture = cam
+import sys
 
-	while True:
-	    # Capture frame-by-frame
-	    ret, frame = video_capture.read()
-	    if ret:
-	        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#import face cascades
+faceCascade1 = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_alt2.xml')
+faceCascade2 = cv2.CascadeClassifier('haarcascades/haarcascade_profileface.xml')
+eyecascade1 = cv2.CascadeClassifier('haarcascades/haarcascade_eye.xml')
+eyecascade2 = cv2.CascadeClassifier('haarcascades/haarcascade_eye_tree_eyeglasses.xml')
 
-	        faces1 = faceCascade1.detectMultiScale(gray, 1.1, 5)
-	        faces2 = faceCascade2.detectMultiScale(gray, 1.1, 5)
-	        # Draw a rectangle around the faces
-	        for (x, y, w, h) in faces1:
-	            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
-	            roi_gray = gray[y:y+h, x:x+w]
-	            roi_color = frame[y:y+h, x:x+w]
+frame = cv2.imread('Test.jpg')
+gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+faces1 = faceCascade1.detectMultiScale(gray, 1.1, 5)
+faces2 = faceCascade2.detectMultiScale(gray, 1.1, 5)
+# Draw a rectangle around the faces
+flag = 0
+for (x, y, w, h) in faces1:
+    cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
+    eyes1 = eyecascade1.detectMultiScale(gray, 1.5, 6)
+    eyes2 = eyecascade2.detectMultiScale(gray, 1.5, 6)
+    for (x1, y1, w1, h1 ) in eyes1:
+        cv2.rectangle(frame, (x1, y1), (x1+w1, y1+h1), (128, 0, 127), 2)
+        split = frame[y1:y1+h1,x1:x1+w1,]
+        flag = 1
+    if flag == 0:
+        for (x2, y2, w2, h2 ) in eyes2:
+            cv2.rectangle(frame, (x2, y2), (x2+w2, y2+h2), (0, 128, 127), 2)
+            flag = 1
+            split = frame[y2:y2+h2,x2:x2+w2]
+        if flag == 0:
+            for (x, y, w, h) in faces2:
+                cv2.rectangle(frame, (x, y), (x+w, y+h), (60, 60, 135), 2)
+                eyes1 = eyecascade1.detectMultiScale(gray, 1.7, 6)
+                eyes2 = eyecascade2.detectMultiScale(gray, 1.7, 6)
+                for (x1, y1, w1, h1 ) in eyes1:
+                    cv2.rectangle(frame, (x1, y1), (x1+w1, y1+h1), (111, 111, 32), 2)
+                    flag = 1
+                    split = frame[y1:y1+h1, x1:x1+w1]
+                if flag == 0 :
+                    for (x2, y2, w2, h2 ) in eyes2:
+                        cv2.rectangle(frame, (x2, y2), (x2+w2, y2+h2), (111, 32, 111), 2)
+                        split = frame[y2:y2+h2,x2:x2+w2]
+cv2.imwrite('Result.jpg', frame);
