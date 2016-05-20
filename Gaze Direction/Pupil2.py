d340524ae8fad89a8e04b0b99eeaa8abd709d315
Soumitra <agarwalsoumitra1504@gmail.com>
2016-05-20 11:38:05 +0530

    Add video capture

--- Gaze Direction/Pupil2.py
+++ Gaze Direction/Pupil2.py
@@ -5,16 +5,31 @@ def get_cascades():
 	faceCascade2 = cv2.CascadeClassifier('haarcascades/haarcascade_profileface.xml')
 	eyecascade1 = cv2.CascadeClassifier('haarcascades/haarcascade_eye.xml')
 	eyecascade2 = cv2.CascadeClassifier('haarcascades/haarcascade_eye_tree_eyeglasses.xml')
-
+def video_capture():
+	cam = cv2.VideoCapture(-1)
+	cam.set(3,640)
+	cam.set(4,480)
+	video_capture = cam
+	return video_capture
 #define main function
 def pupil_detect():
-	get_cascades
-	frame = cv2.imread('Test_Images/Test8.jpg')
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	get_cascades()
+	video_capture = video_capture()
+	cam = cv2.VideoCapture(-1)
+	cam.set(3,640)
+	cam.set(4,480)
+	video_capture = cam
 
-	faces1 = faceCascade1.detectMultiScale(gray, 1.1, 5)
-	faces2 = faceCascade2.detectMultiScale(gray, 1.1, 5)
-	for (x, y, w, h) in faces1:
-	    cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
-	    roi_gray = gray[y:y+h, x:x+w]
-	    roi_color = frame[y:y+h, x:x+w]
+	while True:
+	    # Capture frame-by-frame
+	    ret, frame = video_capture.read()
+	    if ret:
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        faces1 = faceCascade1.detectMultiScale(gray, 1.1, 5)
+        faces2 = faceCascade2.detectMultiScale(gray, 1.1, 5)
+        # Draw a rectangle around the faces
+        for (x, y, w, h) in faces1:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = frame[y:y+h, x:x+w]
