57ed2c524763eeb3a74e58095d9e8422acab9a84
root <agarwalsoumitra1504@gmail.com>
2016-05-20 21:30:25 +0530

    Hough circle

--- Gaze Direction/Pupil2.py
+++ Gaze Direction/Pupil2.py
@@ -1,5 +1,6 @@
 import cv2
 import sys
+import numpy as np
 
 #import face cascades
 faceCascade1 = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_alt2.xml')
@@ -19,19 +20,33 @@ for (x, y, w, h) in faces1:
     eyes1 = eyecascade1.detectMultiScale(gray, 1.5, 6)
     eyes2 = eyecascade2.detectMultiScale(gray, 1.5, 6)
     for (x1, y1, w1, h1 ) in eyes1:
-        cv2.rectangle(frame, (x1, y1), (x1+w1, y1+h1), (128, 0, 127), 2)
-        split = frame[y1:y1+h1,x1:x1+w1]
-        split = cv2.Canny(split,100,200)
-        # frame[y1:y1+h1,x1:x1+w1]= split        
-        flag = 1
+		cv2.rectangle(frame, (x1, y1), (x1+w1, y1+h1), (128, 0, 127), 2)
+		split = frame[y1:y1+h1,x1:x1+w1]
+		split = cv2.Canny(split,100,200)
+		circles = cv2.HoughCircles(split,cv2.HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=0,maxRadius=0)
+		circles = np.uint16(np.around(circles))
+		flag = 1
+		for i in circles[0,:]:
+			cv2.circle(split,(i[0],i[1]),i[2],(0,255,0),2)
+			cv2.circle(split,(i[0],i[1]),2,(0,0,255),3)
+			print(i)
     if flag == 0:
         for (x2, y2, w2, h2 ) in eyes2:
-            cv2.rectangle(frame, (x2, y2), (x2+w2, y2+h2), (0, 128, 127), 2)
-            flag = 1
-            split = frame[y2:y2+h2,x2:x2+w2]
-        if flag == 0:
+			cv2.rectangle(frame, (x2, y2), (x2+w2, y2+h2), (0, 128, 127), 2)
+			flag = 1
+			split = frame[y2:y2+h2,x2:x2+w2]
+			split = cv2.Canny(split,100,200)
+			circles = cv2.HoughCircles(split,cv2.HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=0,maxRadius=0)
+			circles = np.uint16(np.around(circles))
+			for i in circles[0,:]:
+				print(i)
+				#draw the outer circle
+				cv2.circle(split,(i[0],i[1]),i[2],(0,255,0),2)
+				# draw the center of the circle
+				cv2.circle(split,(i[0],i[1]),2,(0,0,255),3)
+		if flag == 0:
             for (x, y, w, h) in faces2:
-                cv2.rectangle(frame, (x, y), (x+w, y+h), (60, 60, 135), 2)
+                cv2.rectangle(frame, (x, y), (x+herew, y+h), (60, 60, 135), 2)
                 eyes1 = eyecascade1.detectMultiScale(gray, 1.7, 6)
                 eyes2 = eyecascade2.detectMultiScale(gray, 1.7, 6)
                 for (x1, y1, w1, h1 ) in eyes1:
