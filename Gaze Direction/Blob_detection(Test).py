c7a590460561f1cd4774d789be49f6df3c6016bc
root <agarwalsoumitra1504@gmail.com>
2016-05-24 02:55:46 +0530

    Detecting blob centres

--- Gaze Direction/Blob_detection(Test).py
+++ Gaze Direction/Blob_detection(Test).py
@@ -3,36 +3,10 @@ import cv2
 import numpy as np;
 
 # Read image
-im = cv2.imread("Result_Images/Result1.jpg", cv2.IMREAD_GRAYSCALE)
-# Setup SimpleBlobDetector parameters.
-params = cv2.SimpleBlobDetector_Params()
+im = cv2.imread("Result_Images/eye.jpg", cv2.IMREAD_GRAYSCALE)
 
-# Change thresholds
-params.minThreshold = 10;
-params.maxThreshold = 200;
-
-# Filter by Area.
-params.filterByArea = True
-params.minArea = 1500
-
-# Filter by Circularity
-params.filterByCircularity = True
-params.minCircularity = 0.1
-
-# Filter by Convexity
-params.filterByConvexity = True
-params.minConvexity = 0.87
-
-# Filter by Inertia
-params.filterByInertia = True
-params.minInertiaRatio = 0.01
-
-# Create a detector with the parameters
-ver = (cv2.__version__).split('.')
-if int(ver[0]) < 3 :
-	detector = cv2.SimpleBlobDetector(params)
-else : 
-	detector = cv2.SimpleBlobDetector_create(params)
+# Set up the detector with default parameters.
+detector = cv2.SimpleBlobDetector_create()
 
 # Detect blobs.
 keypoints = detector.detect(im)
