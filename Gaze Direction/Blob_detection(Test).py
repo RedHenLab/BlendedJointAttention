54252dbbd1bff7e65695498565260dcf0868c9ca
root <agarwalsoumitra1504@gmail.com>
2016-05-24 02:31:47 +0530

    add params

--- Gaze Direction/Blob_detection(Test).py
+++ Gaze Direction/Blob_detection(Test).py
@@ -3,10 +3,36 @@ import cv2
 import numpy as np;
 
 # Read image
-im = cv2.imread("eye.jpg", cv2.IMREAD_GRAYSCALE)
+im = cv2.imread("Result_Images/Result1.jpg", cv2.IMREAD_GRAYSCALE)
+# Setup SimpleBlobDetector parameters.
+params = cv2.SimpleBlobDetector_Params()
 
-# Set up the detector with default parameters.
-detector = cv2.SimpleBlobDetector()
+# Change thresholds
+params.minThreshold = 10;
+params.maxThreshold = 200;
+
+# Filter by Area.
+params.filterByArea = True
+params.minArea = 1500
+
+# Filter by Circularity
+params.filterByCircularity = True
+params.minCircularity = 0.1
+
+# Filter by Convexity
+params.filterByConvexity = True
+params.minConvexity = 0.87
+
+# Filter by Inertia
+params.filterByInertia = True
+params.minInertiaRatio = 0.01
+
+# Create a detector with the parameters
+ver = (cv2.__version__).split('.')
+if int(ver[0]) < 3 :
+	detector = cv2.SimpleBlobDetector(params)
+else : 
+	detector = cv2.SimpleBlobDetector_create(params)
 
 # Detect blobs.
 keypoints = detector.detect(im)
@@ -16,5 +42,4 @@ keypoints = detector.detect(im)
 im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
 # Show keypoints
-cv2.imshow("Keypoints", im_with_keypoints)
-cv2.waitKey(0)
+cv2.imwrite('Result_Images/Keypoints.jpg', im_with_keypoints)
