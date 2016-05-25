432f67e57449c690eeb9a0b2ba9016ed10d49be4
root <agarwalsoumitra1504@gmail.com>
2016-05-26 01:33:04 +0530

    Check type of sobelx64f

--- Gaze Direction/Fabian Timm and Erhardt Barth/Test.py
+++ Gaze Direction/Fabian Timm and Erhardt Barth/Test.py
@@ -9,6 +9,7 @@ sobelx8u = cv2.Sobel(img,cv2.CV_8U,1,0,ksize=5)
 
 # Output dtype = cv2.CV_64F. Then take its absolute and convert to cv2.CV_8U
 sobelx64f = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=5)
+print(type(sobelx64f))
 abs_sobel64f = np.absolute(sobelx64f)
 sobel_8u = np.uint8(abs_sobel64f)
 
