0fc504b3804652a3098e91d4f13467aeb09ef2cb
Soumitra <agarwalsoumitra1504@gmail.com>
2016-05-27 16:36:17 +0530

    Replace with corrected Fabian

--- Gaze Direction/Gaze_direction.py
+++ Gaze Direction/Gaze_direction.py
@@ -12,8 +12,8 @@ video_capture = cam
 
 #extract eye image
 
-faceCascade1 = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_alt2.xml')
-eyecascade1 = cv2.CascadeClassifier('haarcascades/haarcascade_eye.xml')
+faceCascade1 = cv2.CascadeClassifier('../haarcascades/haarcascade_frontalface_alt2.xml')
+eyecascade1 = cv2.CascadeClassifier('../haarcascades/haarcascade_eye.xml')
 
 while True:
     # Capture frame-by-frame
@@ -27,21 +27,21 @@ while True:
 		    for (x1, y1, w1, h1 ) in eyes1:
 				split = frame[y1:y1+h1,x1:x1+w1]
 				split1=cv2.cvtColor(split, cv2.COLOR_BGR2GRAY)
-				sob = cv2.Laplacian(split1,cv2.CV_64F)
-				minin = -1
+				sobelx = cv2.Sobel(split1,cv2.CV_64F,1,0,ksize=5)
+				sobely = cv2.Sobel(split1,cv2.CV_64F,0,1,ksize=5)
+				sob = np.multiply(sobelx,sobely)
+				minin = 100000
 				minj=0
 				mini=0
 				print(sob.shape)
 				for i in range(len(sob)/2):
 					for j in range(len(sob)/2):
-						if(sob[int(i+len(sob)/4)][int(j+len(sob)/4)]>minin):
+						if(sob[int(i+len(sob)/4)][int(j+len(sob)/4)]<minin):
 							minin=gray[i][j]
 							mini=i
 							minj=j
 							print(mini,minj)
-				cv2.circle(frame,(x1+mini,y1+minj),4,(0,0,255))	
-				cv2.circle(frame,(x1,y1+h1/2),4,(0,0,255))
-				cv2.circle(frame,(x1+w1,y1+h1/2),4,(0,0,255))					
+				cv2.circle(frame,(x1+mini,y1+minj),4,(0,0,255))						
 
 		# Display the resulting frame
         cv2.imshow('Video', frame)
