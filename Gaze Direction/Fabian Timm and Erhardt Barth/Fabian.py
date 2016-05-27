3396b11d45867c5f4e36fd938a36aeb8001c0cd8
Soumitra <agarwalsoumitra1504@gmail.com>
2016-05-27 16:29:35 +0530

    Try maximum with this as well

--- Gaze Direction/Fabian Timm and Erhardt Barth/Fabian.py
+++ Gaze Direction/Fabian Timm and Erhardt Barth/Fabian.py
@@ -32,13 +32,13 @@ while True:
 				sobelx = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=5)
 				sobely = cv2.Sobel(img,cv2.CV_64F,0,1,ksize=5)
 				sob = np.multiply(sobelx,sobely)
-				minin = -1
+				minin = 100000
 				minj=0
 				mini=0
 				print(sob.shape)
 				for i in range(len(sob)/2):
 					for j in range(len(sob)/2):
-						if(sob[int(i+len(sob)/4)][int(j+len(sob)/4)]>minin):
+						if(sob[int(i+len(sob)/4)][int(j+len(sob)/4)]<minin):
 							minin=gray[i][j]
 							mini=i
 							minj=j
