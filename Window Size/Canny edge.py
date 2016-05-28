b85f659f66f34d70b5b574ecd41cb16a7da80a90
Soumitra <agarwalsoumitra1504@gmail.com>
2016-05-28 11:08:10 +0530

    Add video read and canny code

--- Window Size/Canny edge.py
+++ Window Size/Canny edge.py
@@ -1,3 +1,23 @@
 import cv2
 import sys
 import numpy as np
+from matplotlib import pyplot as plt
+
+# Video capture via webcam
+cam = cv2.VideoCapture(-1)
+cam.set(3,640)
+cam.set(4,480)
+video_capture = cam
+while True:
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+    if ret:
+
+    	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    	frame = cv2.Canny(gray,100,200)
+    	cv2.imshow('Video', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+# Release video capture
+video_capture.release()
+cv2.destroyAllWindows()
