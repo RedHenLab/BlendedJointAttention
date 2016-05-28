a3b850f732f8cead2b0e9159d56ad342424c5b80
Soumitra <agarwalsoumitra1504@gmail.com>
2016-05-28 11:26:09 +0530

    Update params

--- Window Size/Contours.py
+++ Window Size/Contours.py
@@ -14,9 +14,10 @@ while True:
     if ret:
 
     	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    	frame = cv2.Canny(gray,100,90)
+    	gray = cv2.Canny(gray,100,180)
     	_, contours,_ = cv2.findContours(frame,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    	cv2.drawContours(frame, contours, -1, (0,255,0), 3)
+    	print(len(contours))
+    	# cv2.drawContours(frame, contours, -1, (0,255,0), 3)
     	cv2.imshow('Video', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
