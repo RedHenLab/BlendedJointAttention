cf982837e84c719b82d4296544598d76d475b075
Soumitra <agarwalsoumitra1504@gmail.com>
2016-05-31 02:35:04 +0530

    Add robust code for image testing

---
+++ Window Size/shapedetection2_test.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('star.jpg',0)
+ret,thresh = cv2.threshold(img,127,255,0)
+contours,hierarchy = cv2.findContours(thresh, 1, 2)
+
+cnt = contours[0]
+M = cv2.moments(cnt)
+print M
+
+cx = int(M['m10']/M['m00'])
+cy = int(M['m01']/M['m00'])
