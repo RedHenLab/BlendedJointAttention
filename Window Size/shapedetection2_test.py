5437d4feb4139b972dc9d60a3de8fa547e3af955
Soumitra <agarwalsoumitra1504@gmail.com>
2016-05-31 03:02:40 +0530

    Find all contours

--- Window Size/shapedetection2_test.py
+++ Window Size/shapedetection2_test.py
@@ -5,14 +5,16 @@ img = cv2.imread('Test_Images/Cube.jpg', 0)
 ret,thresh = cv2.threshold(img,127,255,0)
 _, contours,hierarchy = cv2.findContours(thresh, 1, 2)
 
-cnt = contours[0]
-M = cv2.moments(cnt)
-print M
+for i in len(coutours):
+	cnt = contours[i]
+	M = cv2.moments(cnt)
+	# print M
 
-cx = int(M['m10']/M['m00'])
-cy = int(M['m01']/M['m00'])
+	cx = int(M['m10']/M['m00'])
+	cy = int(M['m01']/M['m00'])
 
-epsilon = 0.1*cv2.arcLength(cnt,True)
-approx = cv2.approxPolyDP(cnt,epsilon,True)
 
-print len(approx)
+	epsilon = 0.1*cv2.arcLength(cnt,True)
+	approx = cv2.approxPolyDP(cnt,epsilon,True)
+
+	print len(approx)
