2fa484fc35f9a4777c7679664eb0fff9b0eb3640
Soumitra <agarwalsoumitra1504@gmail.com>
2016-05-31 03:09:51 +0530

    Convert only the test image to grey

--- Window Size/shapedetection2_test.py
+++ Window Size/shapedetection2_test.py
@@ -3,6 +3,7 @@ import numpy as np
 
 img = cv2.imread('Test_Images/Cube.jpg')
 img1 = img
+
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 ret,thresh = cv2.threshold(img,127,255,0)
 _, contours,hierarchy = cv2.findContours(thresh, 1, 2)
