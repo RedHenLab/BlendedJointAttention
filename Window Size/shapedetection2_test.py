71c35f888331f8c2e3c19c2e8950b5539e2b61be
Soumitra <agarwalsoumitra1504@gmail.com>
2016-05-31 02:46:20 +0530

    Add path to image

--- Window Size/shapedetection2_test.py
+++ Window Size/shapedetection2_test.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('star.jpg',0)
+img = cv2.imread('/Test_Images/cube.jpg',0)
 ret,thresh = cv2.threshold(img,127,255,0)
 contours,hierarchy = cv2.findContours(thresh, 1, 2)
 
@@ -15,3 +15,4 @@ cy = int(M['m01']/M['m00'])
 epsilon = 0.1*cv2.arcLength(cnt,True)
 approx = cv2.approxPolyDP(cnt,epsilon,True)
 
+print len(approx)
