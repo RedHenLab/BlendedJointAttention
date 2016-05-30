5afabddb8e0fd6109c936d082cd411a3e483b49f
Soumitra <agarwalsoumitra1504@gmail.com>
2016-05-31 03:09:27 +0530

    Update contour size

--- Window Size/shapedetection2_test.py
+++ Window Size/shapedetection2_test.py
@@ -1,11 +1,12 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('Test_Images/Cube.jpg', 0)
+img = cv2.imread('Test_Images/Cube.jpg')
+img1 = img
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 ret,thresh = cv2.threshold(img,127,255,0)
 _, contours,hierarchy = cv2.findContours(thresh, 1, 2)
 
-img1 = img
 for i in range(len(contours)):
 	cnt = contours[i]
 	M = cv2.moments(cnt)
@@ -19,6 +20,6 @@ for i in range(len(contours)):
 		epsilon = 0.1*cv2.arcLength(cnt,True)
 		approx = cv2.approxPolyDP(cnt,epsilon,True)
 		if(len(approx)==4):
-			cv2.drawContours(img1, contours[i], -1, (0,0,255), 1)
+			cv2.drawContours(img1, contours[i], -1, (0,0,255), 3)
 
 cv2.imwrite('Result_Images/Cube.jpg', img1)
