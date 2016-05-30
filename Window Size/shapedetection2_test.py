774c7942c1cf82474748188704cdad79383a4414
Soumitra <agarwalsoumitra1504@gmail.com>
2016-05-31 03:06:37 +0530

    Add temp image copy

--- Window Size/shapedetection2_test.py
+++ Window Size/shapedetection2_test.py
@@ -5,6 +5,7 @@ img = cv2.imread('Test_Images/Cube.jpg', 0)
 ret,thresh = cv2.threshold(img,127,255,0)
 _, contours,hierarchy = cv2.findContours(thresh, 1, 2)
 
+img1 = img
 for i in range(len(contours)):
 	cnt = contours[i]
 	M = cv2.moments(cnt)
@@ -17,4 +18,5 @@ for i in range(len(contours)):
 
 		epsilon = 0.1*cv2.arcLength(cnt,True)
 		approx = cv2.approxPolyDP(cnt,epsilon,True)
-		print len(approx)
+		if(len(approx)==4):
+			cv2.drawContours(frame, cont[i], -1, (0,0,255), 1)
