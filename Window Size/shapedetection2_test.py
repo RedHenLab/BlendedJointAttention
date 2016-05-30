2224fc58c104766e710646b057ad9521b1cb740c
Soumitra <agarwalsoumitra1504@gmail.com>
2016-05-31 03:04:07 +0530

    Correct range

--- Window Size/shapedetection2_test.py
+++ Window Size/shapedetection2_test.py
@@ -5,11 +5,11 @@ img = cv2.imread('Test_Images/Cube.jpg', 0)
 ret,thresh = cv2.threshold(img,127,255,0)
 _, contours,hierarchy = cv2.findContours(thresh, 1, 2)
 
-for i in len(contours):
+for i in range(len(contours)):
 	cnt = contours[i]
 	M = cv2.moments(cnt)
 	# print M
-
+	
 	cx = int(M['m10']/M['m00'])
 	cy = int(M['m01']/M['m00'])
 
