1d46cf0fb5a59e65ff91b124848ea3fb2232e959
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-03 05:15:57 +0530

    Add parsing code

--- Gesture Recognition/Audio.py
+++ Gesture Recognition/Audio.py
@@ -1,40 +1,39 @@
-import cv2
-import sys
+import glob     # Import glob to easily loop over files
+import pympi    # Import pympi to work with elan files
+import string   # Import string to get the punctuation data
 
-# Defining cascade variables
-faceCascade1 = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_alt2.xml')
-nosecascade = cv2.CascadeClassifier('haarcascades/haarcascade_mcs_nose.xml')
-
-# Video capture via webcam
-cam = cv2.VideoCapture(-1)
-cam.set(3,640)
-cam.set(4,480)
-video_capture = cam
-a = list()
-b = list()
-while True:
-    # Capture frame-by-frame
-    ret, frame = video_capture.read()
-    if ret:
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-        faces1 = faceCascade1.detectMultiScale(gray, 1.1, 5)
-        # Draw a rectangle around the faces
-        for (x, y, w, h) in faces1:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
-            roi_gray = gray[y:y+h, x:x+w]
-            roi_color = frame[y:y+h, x:x+w]
-            nose = nosecascade.detectMultiScale(roi_gray,1.3,5)
-            for (ex,ey,ew,eh) in nose:
-               cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,0,0),2)
-               a.append(ex+ew/2)
-               b.append(ey+eh/2)
-        # Display the resulting frame
-        for i in range(len(a)):
-            cv2.circle(frame, (x+a[i],y+b[i]),1,(128,0,127),2)
-        cv2.imshow('Video', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-# Release video capture
-video_capture.release()
-cv2.destroyAllWindows()
+# Define some variables for later use
+corpus_root = 'Test_EAF/'
+corpus_root1 = 'Result_files'
+print glob.glob("Result_files/*")
+output_file = '{}/annotation_with_timestamp.txt'.format(corpus_root1)
+ws = list()
+time_start = list()
+time_end = list()
+for file_path in glob.glob('{}/*.eaf'.format(corpus_root)):
+    # Initialize the elan file
+    eafob = pympi.Elan.Eaf(file_path)
+    ort_tier_names = eafob.get_tier_names()
+    for ort_tier in ort_tier_names:
+        print(ort_tier)
+        for annotation in eafob.get_annotation_data_for_tier(ort_tier):
+            utterance = annotation[2]
+            words = utterance.split()
+            for word in words:
+                # Remove the possible punctuation
+                
+                for char in string.punctuation:
+                    word = word.replace(char, '')
+                # Convert to lowercase
+                word = word.lower()
+                ws.append(word)
+                time_start.append(annotation[0])
+                time_end.append(annotation[1])
+# print(time_start)
+with open(output_file, 'w') as output_file:
+    # Loop throught the words with their frequencies, we do this sorted because
+    # the file will then be more easily searchable
+    for i in range(len(ws)):
+        # We write the output separated by tabs
+        output_file.write('{}\t{}\t{}\n'.format(time_start[i], time_end[i], ws[i]))
+        
