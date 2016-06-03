5cb7911c03eff9448141824ad5be6394cf77464b
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-03 16:51:46 +0530

    Make indent

--- Gesture Recognition/Dlib(3d).py
+++ Gesture Recognition/Dlib(3d).py
@@ -58,26 +58,25 @@ cam.set(4,480)
 video_capture = cam
 
 while True:
-    print("Processing file: {}".format(f))
-    img = io.imread(f)
+    ret, frame = video_capture.read()
+    if ret:
+	    win.clear_overlay()
+	    win.set_image(img)
 
-    win.clear_overlay()
-    win.set_image(img)
+	    # Ask the detector to find the bounding boxes of each face. The 1 in the
+	    # second argument indicates that we should upsample the image 1 time. This
+	    # will make everything bigger and allow us to detect more faces.
+	    dets = detector(img, 1)
+	    print("Number of faces detected: {}".format(len(dets)))
+	    for k, d in enumerate(dets):
+	        print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(
+	            k, d.left(), d.top(), d.right(), d.bottom()))
+	        # Get the landmarks/parts for the face in box d.
+	        shape = predictor(img, d)
+	        print("Part 0: {}, Part 1: {} ...".format(shape.part(0),
+	                                                  shape.part(1)))
+	        # Draw the face landmarks on the screen.
+	        win.add_overlay(shape)
 
-    # Ask the detector to find the bounding boxes of each face. The 1 in the
-    # second argument indicates that we should upsample the image 1 time. This
-    # will make everything bigger and allow us to detect more faces.
-    dets = detector(img, 1)
-    print("Number of faces detected: {}".format(len(dets)))
-    for k, d in enumerate(dets):
-        print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(
-            k, d.left(), d.top(), d.right(), d.bottom()))
-        # Get the landmarks/parts for the face in box d.
-        shape = predictor(img, d)
-        print("Part 0: {}, Part 1: {} ...".format(shape.part(0),
-                                                  shape.part(1)))
-        # Draw the face landmarks on the screen.
-        win.add_overlay(shape)
-
-    win.add_overlay(dets)
-    dlib.hit_enter_to_continue()
+	    win.add_overlay(dets)
+	    dlib.hit_enter_to_continue()
