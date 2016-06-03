e1f8d5b7c9e4fe7783e30750a045f0d9bbf4509e
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-03 10:38:51 +0530

    Use one classifier loading

--- Gesture Recognition/Dlib(3d).py
+++ Gesture Recognition/Dlib(3d).py
@@ -10,17 +10,17 @@ cam.set(3,640)
 cam.set(4,480)
 video_capture = cam
 
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor('../dlibcascades/shape_predictor_68_face_landmarks.dat')
+		
 while True:
     # Capture frame-by-frame
     ret, frame = video_capture.read()
     if ret:
-		detector = dlib.get_frontal_face_detector()
-		predictor = dlib.shape_predictor('../dlibcascades/shape_predictor_68_face_landmarks.dat')
-		win = dlib.image_window()
 		dets = detector(frame, 1)
 		for k, d in enumerate(dets):
-			shape = predictor(img, d)
-	        print(shape)
+			shape = predictor(frame, d)
+	        # print(shape)
         # Display the resulting frame
 		cv2.imshow('Video', frame)
 		if cv2.waitKey(1) & 0xFF == ord('q'):
