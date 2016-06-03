66002fac1e4bbcf9f51af99785ca3d3a72c1c68e
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-03 16:52:43 +0530

    Complete overlay

--- Gesture Recognition/Dlib(3d).py
+++ Gesture Recognition/Dlib(3d).py
@@ -61,21 +61,15 @@ while True:
     ret, frame = video_capture.read()
     if ret:
 	    win.clear_overlay()
-	    win.set_image(img)
+	    win.set_image(frame)
 
 	    # Ask the detector to find the bounding boxes of each face. The 1 in the
 	    # second argument indicates that we should upsample the image 1 time. This
 	    # will make everything bigger and allow us to detect more faces.
-	    dets = detector(img, 1)
-	    print("Number of faces detected: {}".format(len(dets)))
+	    dets = detector(frame, 1)
 	    for k, d in enumerate(dets):
-	        print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(
-	            k, d.left(), d.top(), d.right(), d.bottom()))
 	        # Get the landmarks/parts for the face in box d.
-	        shape = predictor(img, d)
-	        print("Part 0: {}, Part 1: {} ...".format(shape.part(0),
-	                                                  shape.part(1)))
-	        # Draw the face landmarks on the screen.
+	        shape = predictor(frame, d)
 	        win.add_overlay(shape)
 
 	    win.add_overlay(dets)
