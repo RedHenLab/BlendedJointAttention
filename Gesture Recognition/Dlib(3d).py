37eb2a3fab5aa244b8071784b9b5d6f1a0ffe84c
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-04 06:34:58 +0530

    Eye terminal cv2 circle

--- Gesture Recognition/Dlib(3d).py
+++ Gesture Recognition/Dlib(3d).py
@@ -23,7 +23,7 @@ while True:
 	        # Get the landmarks/parts for the face in box d.
 	        shape = predictor(frame, d)
 	        cv2.circle(frame,(shape.part(36),2,(0,0,255))
-	        cv2.circle(frame,(shape.part(36),4,(0,0,255))
-	        cv2.circle(frame,(shape.part(36),4,(0,0,255))
-	        cv2.circle(frame,(shape.part(36),4,(0,0,255))		
+	        cv2.circle(frame,(shape.part(39),4,(0,0,255))
+	        cv2.circle(frame,(shape.part(42),4,(0,0,255))
+	        cv2.circle(frame,(shape.part(45),4,(0,0,255))		
 	        # win.add_overlay(shape)
