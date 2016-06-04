6001d8424863fab1549a540815ff10d8327ea235
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-04 07:10:47 +0530

    Remove useless window overlay command

--- Gaze Direction/Dlib(3d).py
+++ Gaze Direction/Dlib(3d).py
@@ -5,7 +5,6 @@ import cv2
 
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor('../dlibcascades/shape_predictor_68_face_landmarks.dat')
-win = dlib.image_window()
 
 cam = cv2.VideoCapture(-1)
 cam.set(3,640)
