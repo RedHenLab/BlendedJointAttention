fd04b57f25015f5d5e0369ad6622ced0bc8d234e
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-05 04:47:12 +0530

    Print frame number of detected face

--- Face Detection/Face_Detect_Template_Matching.py
+++ Face Detection/Face_Detect_Template_Matching.py
@@ -68,6 +68,7 @@ while True:
         cv2.imshow('Video', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+        print(frame_number)
 # Release video capture
 video_capture.release()
 cv2.destroyAllWindows()
