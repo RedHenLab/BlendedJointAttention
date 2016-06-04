955d9f061fa5635f2973a63d5b69e8bbdfadc254
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-04 07:30:14 +0530

    Add indent

--- Gaze Direction/Gaze_direction.py
+++ Gaze Direction/Gaze_direction.py
@@ -27,32 +27,24 @@ while True:
 	        cv2.circle(frame,(shape.part(39).x,shape.part(39).y),2,(0,0,255))
 	        cv2.circle(frame,(shape.part(42).x,shape.part(42).y),2,(0,0,255))
 	        cv2.circle(frame,(shape.part(45).x,shape.part(45).y),2,(0,0,255))
-	        cv2.imshow('Video', frame)
-	        if cv2.waitKey(1) & 0xFF == ord('q'):
-	        	break
-    	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    	faces1 = faceCascade1.detectMultiScale(gray, 1.1, 5)
-    	for (x, y, w, h) in faces1:
-		    eyes1 = eyecascade1.detectMultiScale(gray, 1.5, 6)
-		    for (x1, y1, w1, h1 ) in eyes1:
-				split = frame[y1:y1+h1,x1:x1+w1]
-				split1=cv2.cvtColor(split, cv2.COLOR_BGR2GRAY)
-				sobelx = cv2.Sobel(split1,cv2.CV_64F,1,0,ksize=5)
-				sobely = cv2.Sobel(split1,cv2.CV_64F,0,1,ksize=5)
-				sob = np.multiply(sobelx,sobely)
-				minin = 100000
-				minj=0
-				mini=0
-				print(sob.shape)
-				for i in range(len(sob)/2):
-					for j in range(len(sob)/2):
-						if(sob[int(i+len(sob)/4)][int(j+len(sob)/4)]<minin):
-							minin=gray[i][j]
-							mini=i
-							minj=j
-							print(mini,minj)
-				cv2.circle(frame,(x1+mini,y1+minj),1,(0,0,255))						
-				cv2.line(frame,(x1+mini,y1+minj), (x1+2*mini-w1/2,y1+2*minj-h1/2),(255,0,0))
+			split = frame[y1:y1+h1,x1:x1+w1]
+			split1=cv2.cvtColor(split, cv2.COLOR_BGR2GRAY)
+			sobelx = cv2.Sobel(split1,cv2.CV_64F,1,0,ksize=5)
+			sobely = cv2.Sobel(split1,cv2.CV_64F,0,1,ksize=5)
+			sob = np.multiply(sobelx,sobely)
+			minin = 100000
+			minj=0
+			mini=0
+			print(sob.shape)
+			for i in range(len(sob)/2):
+				for j in range(len(sob)/2):
+					if(sob[int(i+len(sob)/4)][int(j+len(sob)/4)]<minin):
+						minin=gray[i][j]
+						mini=i
+						minj=j
+						print(mini,minj)
+			cv2.circle(frame,(x1+mini,y1+minj),1,(0,0,255))						
+			cv2.line(frame,(x1+mini,y1+minj), (x1+2*mini-w1/2,y1+2*minj-h1/2),(255,0,0))
 		# Display the resulting frame
         cv2.imshow('Video', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
