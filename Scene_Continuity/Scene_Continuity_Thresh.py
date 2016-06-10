4cf6196fa035231f9699668945225701942bf4b7
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-11 03:37:59 +0530

    Initial changes

--- Scene_Continuity/Scene_Continuity_Thresh.py
+++ Scene_Continuity/Scene_Continuity_Thresh.py
@@ -1,6 +1,7 @@
 import cv2
+import time 
 
-cam = cv2.VideoCapture("test.mp4")
+cam = cv2.VideoCapture('test.mp4')
 cam.set(3,640)
 cam.set(4,480)
 video_capture = cam
@@ -8,18 +9,29 @@ frame_num = 0
 while True:
     # Capture frame-by-frame
     ret, frame = video_capture.read()
+    time.sleep(0.01)
     if ret:
-    	frame = frame+1
-    	if frame_num == 1:
-    		firstFrame = frame
+		frame_num = frame_num+1
 		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		if frame_num == 1:
+			firstFrame = gray
+		# if frame_num == 10:
+		# 	frame_num=0
 		gray = cv2.GaussianBlur(gray, (21, 21), 0)
 		frameDelta = cv2.absdiff(firstFrame, gray)
 		thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
-		# Display the resulting frame
-		cv2.imshow('Video', thresh)
-		cv2.imshow('Video',	frameDelta)
-		cv2.imshow('Video', gray)
+		thresh = cv2.dilate(thresh, None, iterations=2)
+		_, cnts, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+ 		for c in cnts:
+		# if the contour is too small, ignore it
+			if cv2.contourArea(c) < 10:
+				continue
+	 		(x, y, w, h) = cv2.boundingRect(c)
+			cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+			# Display the resulting frame
+		# cv2.imshow('Video', thresh)
+		# cv2.imshow('Video', frameDelta)
+		cv2.imshow('Video', frame)
 		if cv2.waitKey(1) & 0xFF == ord('q'):
 			break
 		print frame_num
