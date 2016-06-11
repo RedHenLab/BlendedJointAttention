6b499ab6cc6548ee14eec93adbc2d4ca8c30dd60
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-11 20:39:41 +0530

    Detect scene changes

--- Scene_Continuity/Scene_Continuity_Thresh.py
+++ Scene_Continuity/Scene_Continuity_Thresh.py
@@ -39,7 +39,7 @@ while True:
 		cv2.imshow('Video', frame)
 		if cv2.waitKey(1) & 0xFF == ord('q'):
 			break
-		print frame_num
+		# print frame_num
 # Release video capture
 video_capture.release()
 cv2.destroyAllWindows()
