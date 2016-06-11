c69e79cac90e9f79850f9772ecde7b4dc462a9c3
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-11 21:15:49 +0530

    Add datetime

--- Scene_Continuity/Scene_Continuity_Foreground.py
+++ Scene_Continuity/Scene_Continuity_Foreground.py
@@ -1,15 +1,14 @@
 import numpy as np
 import cv2
+import datetime
 
 cap = cv2.VideoCapture('test.mp4')
 cv2.ocl.setUseOpenCL(False)
 fgbg = cv2.createBackgroundSubtractorMOG2()
-
+time = sys.getTime()
 while(1):
     ret, frame = cap.read()
-    
     fgmask = fgbg.apply(frame)
-    
     cv2.imshow('frame',fgmask)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
