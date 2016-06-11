ebd8b4a3d3442e649c6149c41c03124df1b4960f
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-11 21:01:24 +0530

    Add methods

--- Scene_Continuity/Scene_Continuity_Foreground.py
+++ Scene_Continuity/Scene_Continuity_Foreground.py
@@ -1,17 +1,20 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture('test.mp4')
+cap = cv2.VideoCapture('vtest.avi')
 
-fgbg = cv2.createBackgroundSubtractorMOG2()
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
+fgbg = cv2.createBackgroundSubtractorGMG()
 
 while(1):
     ret, frame = cap.read()
-    frame = fgbg.apply(frame)
-    cv2.imshow('Video',frame)
+
+    fgmask = fgbg.apply(frame)
+    fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
+
+    cv2.imshow('frame',fgmask)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
- 
 cap.release()
 cv2.destroyAllWindows()
