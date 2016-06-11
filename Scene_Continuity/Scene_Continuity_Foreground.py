60c7d29ecb99de80e6d33d6fd06e59695a84cd1e
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-11 23:42:32 +0530

    Add frame numbers and last detection:

--- Scene_Continuity/Scene_Continuity_Foreground.py
+++ Scene_Continuity/Scene_Continuity_Foreground.py
@@ -6,19 +6,23 @@ video_capture = cap
 cv2.ocl.setUseOpenCL(False)
 fgbg = cv2.createBackgroundSubtractorMOG2()
 frame_num = 0
+last_detected = -20
 scene_num = 0
 while(1):
+	frame_num = frame_num + 1
     ret, frame = video_capture.read()
     fgmask = fgbg.apply(frame)
     num_white = 0
-    for i in range(fgmask.shape[0]):
-    	for j in range(fgmask.shape[1]):
-    		if fgmask[i][j] == 255:
-    			num_white = num_white+1
-    			if(num_white>0.8*fgmask.shape[0]*fgmask.shape[0]):
-    				scene_num = scene_num + 1
-    				print("Scene changed : ", scene_num)
-    				break
+    if(frame_num-last_detected>20):
+    	last_detected = frame_num
+	    for i in range(fgmask.shape[0]):
+	    	for j in range(fgmask.shape[1]):
+	    		if fgmask[i][j] == 255:
+	    			num_white = num_white+1
+	    			if(num_white>0.8*fgmask.shape[0]*fgmask.shape[0]):
+	    				scene_num = scene_num + 1
+	    				print("Scene changed : ", scene_num)
+	    				break
     cv2.imshow('Video',fgmask)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
