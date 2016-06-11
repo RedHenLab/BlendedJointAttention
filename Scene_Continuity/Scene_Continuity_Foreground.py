e6bfcdc0ed12f7e2c6054d07166e3aeb29957062
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-11 22:40:59 +0530

    Add resize for capture

--- Scene_Continuity/Scene_Continuity_Foreground.py
+++ Scene_Continuity/Scene_Continuity_Foreground.py
@@ -3,6 +3,8 @@ import cv2
 import datetime
 
 cap = cv2.VideoCapture('test.mp4')
+cap.set(3,640)
+cap.set(4,480)
 cv2.ocl.setUseOpenCL(False)
 fgbg = cv2.createBackgroundSubtractorMOG2()
 time = datetime.datetime.now().time()
@@ -10,6 +12,7 @@ print time
 while(1):
     ret, frame = cap.read()
     fgmask = fgbg.apply(frame)
+    print(fgmask.shape)
     cv2.imshow('frame',fgmask)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
