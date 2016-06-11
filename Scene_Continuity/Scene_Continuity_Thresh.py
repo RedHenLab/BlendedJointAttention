db988ff773ddd84f49cd4ec44e8bf9c5634063a8
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-11 20:36:45 +0530

    Correct contour area

--- Scene_Continuity/Scene_Continuity_Thresh.py
+++ Scene_Continuity/Scene_Continuity_Thresh.py
@@ -29,7 +29,7 @@ while True:
 				continue
 	 		(x, y, w, h) = cv2.boundingRect(c)
 			cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-			if cv2.contourArea(c) > 300000:
+			if cv2.contourArea(c) > 200000:
 				frame_num = 0
 				scene_num = scene_num+1
 				print(scene_num)
