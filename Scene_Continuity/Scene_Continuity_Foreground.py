5680f2051189363f006b8de60aa734e76c980cf2
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-11 21:00:05 +0530

    Minor bug correction

--- Scene_Continuity/Scene_Continuity_Foreground.py
+++ Scene_Continuity/Scene_Continuity_Foreground.py
@@ -7,10 +7,8 @@ fgbg = cv2.createBackgroundSubtractorMOG2()
 
 while(1):
     ret, frame = cap.read()
-
-    fgmask = fgbg.apply(frame)
-
-    cv2.imshow('frame',fgmask)
+    frame = fgbg.apply(frame)
+    cv2.imshow('Video',frame)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
