e1fcfc4a3214ac2b4263c2ffa39e69ec6127934a
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-11 23:14:30 +0530

    Add time parameter

--- Scene_Continuity/Scene_Continuity_Foreground.py
+++ Scene_Continuity/Scene_Continuity_Foreground.py
@@ -7,19 +7,19 @@ video_capture = cap
 cv2.ocl.setUseOpenCL(False)
 fgbg = cv2.createBackgroundSubtractorMOG2()
 time = datetime.datetime.now().time()
-print time
 scene_num = 0
 while(1):
     ret, frame = video_capture.read()
     fgmask = fgbg.apply(frame)
-    num_zero = 0
-    for i in range(fgmask.shape[0]):
-    	for j in range(fgmask.shape[1]):
-    		if fgmask[i][j] == 0:
-    			num_zero = num_zero+1
-    if(num_zero<0.1*fgmask.shape[0]*fgmask.shape[0]):
-    	scene_num = scene_num + 1
-    	print("Scene changed : ", scene_num)
+    num_black = 0
+    if(datetime.datetime.now().time()-time>1):
+	    for i in range(fgmask.shape[0]):
+	    	for j in range(fgmask.shape[1]):
+	    		if fgmask[i][j] == 255:
+	    			num_black = num_black+1
+	    			if(num_black>0.1*fgmask.shape[0]*fgmask.shape[0]):
+	    				scene_num = scene_num + 1
+	    				print("Scene changed : ", scene_num)
     cv2.imshow('frame',fgmask)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
