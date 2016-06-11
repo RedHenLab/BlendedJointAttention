ba54a259e413a313a57ce3a9016d3a979c992dd6
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-11 21:13:04 +0530

    Corrected background subtraction

--- Scene_Continuity/Scene_Continuity_Foreground.py
+++ Scene_Continuity/Scene_Continuity_Foreground.py
@@ -1,17 +1,15 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture('vtest.avi')
-
-kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
-fgbg = cv2.createBackgroundSubtractorGMG()
+cap = cv2.VideoCapture('test.mp4')
+cv2.ocl.setUseOpenCL(False)
+fgbg = cv2.createBackgroundSubtractorMOG2()
 
 while(1):
     ret, frame = cap.read()
-
+    
     fgmask = fgbg.apply(frame)
-    fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
-
+    
     cv2.imshow('frame',fgmask)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
