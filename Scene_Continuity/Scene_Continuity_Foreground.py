52e57e9bc6091db13fa74c797aab797f72ae668d
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-11 23:23:44 +0530

    Remove time

--- Scene_Continuity/Scene_Continuity_Foreground.py
+++ Scene_Continuity/Scene_Continuity_Foreground.py
@@ -12,17 +12,15 @@ while(1):
     ret, frame = video_capture.read()
     fgmask = fgbg.apply(frame)
     num_white = 0
-    c = datetime.datetime.now()-time 
-    if(c.seconds>1):
-	    time = datetime.datetime.now()
-	    for i in range(fgmask.shape[0]):
-	    	for j in range(fgmask.shape[1]):
-	    		if fgmask[i][j] == 255:
-	    			num_white = num_white+1
-	    			if(num_white>0.8*fgmask.shape[0]*fgmask.shape[0]):
-	    				scene_num = scene_num + 1
-	    				print("Scene changed : ", scene_num)
-	    				break
+    time = datetime.datetime.now()
+    for i in range(fgmask.shape[0]):
+    	for j in range(fgmask.shape[1]):
+    		if fgmask[i][j] == 255:
+    			num_white = num_white+1
+    			if(num_white>0.8*fgmask.shape[0]*fgmask.shape[0]):
+    				scene_num = scene_num + 1
+    				print("Scene changed : ", scene_num)
+    				break
     cv2.imshow('frame',fgmask)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
