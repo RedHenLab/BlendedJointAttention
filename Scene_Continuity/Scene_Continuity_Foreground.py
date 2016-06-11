71d0d298c20422e1c35f6b443ee53785379298dc
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-11 23:11:22 +0530

    Add scene change

--- Scene_Continuity/Scene_Continuity_Foreground.py
+++ Scene_Continuity/Scene_Continuity_Foreground.py
@@ -8,6 +8,7 @@ cv2.ocl.setUseOpenCL(False)
 fgbg = cv2.createBackgroundSubtractorMOG2()
 time = datetime.datetime.now().time()
 print time
+scene_num = 0
 while(1):
     ret, frame = video_capture.read()
     fgmask = fgbg.apply(frame)
@@ -16,6 +17,9 @@ while(1):
     	for j in range(len(fgmask.shape[1])):
     		if fgmask[i][j] == 0:
     			num_zero = num_zero+1
+    if(num_zero<0.1*fgmask.shape[0]*fgmask.shape[0]):
+    	scene_num = scene_num + 1
+    	print("Scene changed : ", scene_num)
     cv2.imshow('frame',fgmask)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
