94925fe660a52e3280836bbde12bbfd0b9d46ad6
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-17 12:37:26 +0530

    Add frame color

--- Gaze Direction/Gaze_direction_cross.py
+++ Gaze Direction/Gaze_direction_cross.py
@@ -44,16 +44,17 @@ while True:
     # Capture frame-by-frame
 	ret, frame = video_capture.read()
 	if ret:
+		frame_color = frame
 		frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 		dets = detector(frame, 1)
 		for k, d in enumerate(dets):
 	        # Get the landmarks/parts for the face in box d.
 			shape = predictor(frame, d)
 	        # print(type(shape.part(1).x))
-			cv2.circle(frame,(shape.part(36).x,shape.part(36).y),2,(0,0,255))
-			cv2.circle(frame,(shape.part(39).x,shape.part(39).y),2,(0,0,255))
-			cv2.circle(frame,(shape.part(42).x,shape.part(42).y),2,(0,0,255))
-			cv2.circle(frame,(shape.part(45).x,shape.part(45).y),2,(0,0,255))
+			cv2.circle(frame_color,(shape.part(36).x,shape.part(36).y),2,(0,0,255))
+			cv2.circle(frame_color,(shape.part(39).x,shape.part(39).y),2,(0,0,255))
+			cv2.circle(frame_color,(shape.part(42).x,shape.part(42).y),2,(0,0,255))
+			cv2.circle(frame_color,(shape.part(45).x,shape.part(45).y),2,(0,0,255))
 			x1 = shape.part(36).x
 			y1 = shape.part(37).y-2
 			x2 = shape.part(39).x
