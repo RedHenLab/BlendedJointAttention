3d7d106290f0c60606bd74b4ada3724753482fbb
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-17 12:49:30 +0530

    Remove split.shape

--- Gaze Direction/Gaze_direction_cross.py
+++ Gaze Direction/Gaze_direction_cross.py
@@ -65,8 +65,6 @@ while True:
 			centre = cross_spread(split)
 			frame[y1:y2,x1:x2]=split
 			cv2.rectangle(frame_color,(x1,y1), (x2,y2), (0, 0, 255), 2)
-			print split.shape
-			# print centre
 			cv2.circle(frame_color,(x1+centre[1],y1+centre[0]),2,(0,0,255))
 			x1 = shape.part(42).x
 			y1 = shape.part(43).y-2
