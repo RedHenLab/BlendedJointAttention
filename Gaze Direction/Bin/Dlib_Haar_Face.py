a8678d3241cae1e309a66aaf43a7ee26d8e49ea8
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-18 14:58:40 +0530

    Correct cascade directory

--- Gaze Direction/Bin/Dlib_Haar_Face.py
+++ Gaze Direction/Bin/Dlib_Haar_Face.py
@@ -11,8 +11,8 @@ cam.set(3,640)
 cam.set(4,480)
 video_capture = cam
 
-faceCascade1 = cv2.CascadeClassifier('../haarcascades/haarcascade_frontalface_alt2.xml')
-predictor = dlib.shape_predictor('../dlibcascades/shape_predictor_68_face_landmarks.dat')
+faceCascade1 = cv2.CascadeClassifier('../../haarcascades/haarcascade_frontalface_alt2.xml')
+predictor = dlib.shape_predictor('../../dlibcascades/shape_predictor_68_face_landmarks.dat')
 
 
 while True:
