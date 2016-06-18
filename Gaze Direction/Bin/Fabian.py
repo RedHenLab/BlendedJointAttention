0dd713a0d5bb8403f73ec1c226fc21fb51f27c48
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-18 18:47:36 +0530

    Random

--- Gaze Direction/Bin/Fabian.py
+++ Gaze Direction/Bin/Fabian.py
@@ -16,18 +16,21 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     faces = faceCascade.detectMultiScale(gray, 1.3, 5)
-
+    eye = np.ndarray([])
     # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
         gray = np.float32(gray)
-
+        eyes = eye_cascade.detectMultiScale(roi_gray,1.3,5)
+        for (ex,ey,ew,eh) in eyes:
+           cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,0,255),2)
+           eye = roi_color[ey:ey+eh, ex:ex+ew]
     # Display the resulting frame
     frame = cv2.flip(frame,1)
 
-    cv2.imshow('Video',frame)
+    cv2.imshow('Video',eye)
     #cv2.imshow('Video', frame)
     fr = fr + 1
     print("Frame #")
