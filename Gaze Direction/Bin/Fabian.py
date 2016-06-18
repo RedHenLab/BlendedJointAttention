8a0158146517603482952fe0086bbf4f4c34faf8
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-18 14:58:18 +0530

    Minor changes

--- Gaze Direction/Bin/Fabian.py
+++ Gaze Direction/Bin/Fabian.py
@@ -23,15 +23,6 @@ while True:
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
         gray = np.float32(gray)
-        #dst = cv2.cornerHarris(gray,2,3,0.04)
-        #result is dilated for marking the corners, not important
-        #dst = cv2.dilate(dst,None)
-        # Threshold for an optimal value, it may vary depending on the image.
-        #frame[dst>0.01*dst.max()]=[0,0,255]
- 
-        eyes = eye_cascade.detectMultiScale(roi_gray,1.3,5)
-        for (ex,ey,ew,eh) in eyes:
-           cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,0,255),2)
 
     # Display the resulting frame
     frame = cv2.flip(frame,1)
