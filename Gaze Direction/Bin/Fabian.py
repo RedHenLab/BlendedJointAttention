cd52206c5761fd06d826acb3deceb84891107a56
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-19 15:49:28 +0530

    Remove schar

--- Gaze Direction/Bin/Fabian.py
+++ Gaze Direction/Bin/Fabian.py
@@ -24,10 +24,6 @@ while True:
             # Get the landmarks/parts for the face in box d.
             shape = predictor(frame, d)
             # print(type(shape.part(1).x))
-            cv2.circle(frame,(shape.part(36).x,shape.part(36).y),2,(0,0,255))
-            cv2.circle(frame,(shape.part(39).x,shape.part(39).y),2,(0,0,255))
-            cv2.circle(frame,(shape.part(42).x,shape.part(42).y),2,(0,0,255))
-            cv2.circle(frame,(shape.part(45).x,shape.part(45).y),2,(0,0,255))
             x1 = shape.part(36).x
             y1 = shape.part(37).y
             x2 = shape.part(39).x
