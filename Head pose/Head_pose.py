e482b3f4d6108571facabff0be85101be00a9554
Soumitra <agarwalsoumitra1504@gmail.com>
2016-07-01 18:03:04 +0530

    Add final y coord

--- Head pose/Head_pose.py
+++ Head pose/Head_pose.py
@@ -25,6 +25,7 @@ while True:
 			mid_y = [(shape.part(27).x+shape.part(66).x)/2, (shape.part(27).y+shape.part(66).y)/2]
 			nose = [shape.part(30).x,shape.part(30).x]
 			final_x = 3*nose[0]-2*mid_x[0]
+			final_y = 3*nose[1]-2*mid_y[1]
 	# Display the resulting frame
 	cv2.imshow('Video', frame)
 	if cv2.waitKey(1) & 0xFF == ord('q'):
