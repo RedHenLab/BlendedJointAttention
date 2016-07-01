a809051216f0de19df896b5a1be2e0b564591c41
Soumitra <agarwalsoumitra1504@gmail.com>
2016-07-01 17:20:05 +0530

    Predictor addition

--- Head pose/Head_pose.py
+++ Head pose/Head_pose.py
@@ -17,7 +17,10 @@ while True:
     # Capture frame-by-frame
 	ret, frame = video_capture.read()
 	if ret:
-		continue
+		dets = detector(frame, 1)
+		for k, d in enumerate(dets):
+		    # Get the landmarks/parts for the face in box d.
+    		shape = predictor(frame, d)
 	# Display the resulting frame
 	cv2.imshow('Video', frame)
 	if cv2.waitKey(1) & 0xFF == ord('q'):
