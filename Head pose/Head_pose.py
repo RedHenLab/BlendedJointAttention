a1ffb5f68dfe858592fc442b7e8b9391dc5bf9c7
Soumitra <agarwalsoumitra1504@gmail.com>
2016-07-01 16:30:20 +0530

    Complete video capture

--- Head pose/Head_pose.py
+++ Head pose/Head_pose.py
@@ -9,3 +9,15 @@ cam = cv2.VideoCapture(-1)
 cam.set(3,640)
 cam.set(4,480)
 video_capture = cam
+
+while True:
+    # Capture frame-by-frame
+	ret, frame = video_capture.read()
+
+	# Display the resulting frame
+    cv2.imshow('Video', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+# Release video capture
+video_capture.release()
+cv2.destroyAllWindows()
