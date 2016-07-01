cecace74d7a6a04b1f7a1df6b1514ff05b5d39d4
Soumitra <agarwalsoumitra1504@gmail.com>
2016-07-01 16:34:30 +0530

    Load predictor

--- Head pose/Head_pose.py
+++ Head pose/Head_pose.py
@@ -10,6 +10,9 @@ cam.set(3,640)
 cam.set(4,480)
 video_capture = cam
 
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor('../dlibcascades/shape_predictor_68_face_landmarks.dat')
+
 while True:
     # Capture frame-by-frame
 	ret, frame = video_capture.read()
