aa2e8e72f8dafac14688a823eea82dd6390f6d2a
Soumitra <agarwalsoumitra1504@gmail.com>
2016-07-12 14:36:38 +0530

    Indent correction

--- Context/lucas_kanade.py
+++ Context/lucas_kanade.py
@@ -24,25 +24,25 @@ while(1):
    ret,frame = cap.read()
    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-   # calculate optical flow
+  # calculate optical flow
    p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
 
-   # Select good points
-    good_new = p1[st==1]
-    good_old = p0[st==1]
+  # Select good points
+  good_new = p1[st==1]
+  good_old = p0[st==1]
 
-    # draw the tracks
-    for i,(new,old) in enumerate(zip(good_new,good_old)):
-        a,b = new.ravel()
-        c,d = old.ravel()
-        mask = cv2.line(mask, (a,b),(c,d), color[i].tolist(), 2)
-        frame = cv2.circle(frame,(a,b),5,color[i].tolist(),-1)
-    img = cv2.add(frame,mask)
+  # draw the tracks
+  for i,(new,old) in enumerate(zip(good_new,good_old)):
+      a,b = new.ravel()
+      c,d = old.ravel()
+      mask = cv2.line(mask, (a,b),(c,d), color[i].tolist(), 2)
+      frame = cv2.circle(frame,(a,b),5,color[i].tolist(),-1)
+  img = cv2.add(frame,mask)
 
-    cv2.imshow('frame',img)
-    k = cv2.waitKey(30) & 0xff
-    if k == 27:
-        break
+  cv2.imshow('frame',img)
+  k = cv2.waitKey(30) & 0xff
+  if k == 27:
+      break
 
     # Now update the previous frame and previous points
     old_gray = frame_gray.copy()
