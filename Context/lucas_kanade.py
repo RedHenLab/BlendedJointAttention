c83c40908a7ac9b7f0f9a6b1ccad1e9442cddc00
Soumitra <agarwalsoumitra1504@gmail.com>
2016-07-12 17:07:48 +0530

    Minor fixes

--- Context/lucas_kanade.py
+++ Context/lucas_kanade.py
@@ -16,7 +16,7 @@ color = np.random.randint(0,255,(100,3))
 ret, old_frame = cap.read()
 print ret
 old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
-p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
+p0 = cv2.goodFeaturesToTrack(old_gray,100,0.01,0)
 
 # Create a mask image for drawing purposes
 mask = np.zeros_like(old_frame)
@@ -36,18 +36,18 @@ while(1):
   for i,(new,old) in enumerate(zip(good_new,good_old)):
       a,b = new.ravel()
       c,d = old.ravel()
-      mask = cv2.line(mask, (a,b),(c,d), color[i].tolist(), 2)
+      mask = cv2.line(frame, (a,b),(c,d), color[i].tolist(), 2)
       frame = cv2.circle(frame,(a,b),5,color[i].tolist(),-1)
   img = cv2.add(frame,mask)
 
-  cv2.imshow('frame',img)
+  cv2.imshow('video',frame)
   k = cv2.waitKey(30) & 0xff
   if k == 27:
       break
 
   # Now update the previous frame and previous points
   old_gray = frame_gray.copy()
-  p0 = good_new.reshape(-1,1,2)
+  # p0 = good_new.reshape(-1,1,2)
 
 cv2.destroyAllWindows()
 cap.release()
