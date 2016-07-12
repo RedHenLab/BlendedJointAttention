812b8724cfcc89f4ad87c6a920e626bc559d6041
Soumitra <agarwalsoumitra1504@gmail.com>
2016-07-12 17:27:58 +0530

    parameter correction

--- Context/lucas_kanade.py
+++ Context/lucas_kanade.py
@@ -4,7 +4,7 @@ import cv2
 cap = cv2.VideoCapture('../Test/test.mp4')
 
 # params for ShiTomasi corner detection
-feature_params = dict( maxCorners = 100, qualityLevel = 0, minDistance = 7, blockSize = 7 )
+feature_params = dict( maxCorners = 100, qualityLevel = 0.1, minDistance = 7, blockSize = 7 )
 
 # Parameters for lucas kanade optical flow
 lk_params = dict( winSize  = (15,15), maxLevel = 2, criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
@@ -16,7 +16,7 @@ color = np.random.randint(0,255,(100,3))
 ret, old_frame = cap.read()
 print ret
 old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
-p0 = cv2.goodFeaturesToTrack(old_gray,100,0.01,0)
+p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
 
 # Create a mask image for drawing purposes
 mask = np.zeros_like(old_frame)
@@ -37,9 +37,9 @@ while(1):
       print i
       a,b = new.ravel()
       c,d = old.ravel()
-      # mask = cv2.line(frame, (a,b),(c,d), color[i].tolist(), 2)
-      # frame = cv2.circle(frame,(a,b),5,color[i].tolist(),-1)
-  # img = cv2.add(frame,mask)
+      mask = cv2.line(mask, (a,b),(c,d), color[i].tolist(), 2)
+      frame = cv2.circle(frame,(a,b),5,color[i].tolist(),-1)
+  img = cv2.add(frame,mask)
 
   cv2.imshow('Video',frame)
   k = cv2.waitKey(30) & 0xff
