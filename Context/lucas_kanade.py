83e49d9f73b884da1a95c0e292f86dbc7bc5c165
Soumitra <agarwalsoumitra1504@gmail.com>
2016-07-12 14:37:21 +0530

    Indent correction

--- Context/lucas_kanade.py
+++ Context/lucas_kanade.py
@@ -21,11 +21,11 @@ p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
 mask = np.zeros_like(old_frame)
 
 while(1):
-   ret,frame = cap.read()
-   frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+  ret,frame = cap.read()
+  frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
   # calculate optical flow
-   p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
+  p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
 
   # Select good points
   good_new = p1[st==1]
