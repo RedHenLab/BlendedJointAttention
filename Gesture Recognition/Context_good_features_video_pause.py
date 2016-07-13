6a70d82f30a31d2bb892f6b7aba1abd65baa477c
Soumitra <agarwalsoumitra1504@gmail.com>
2016-07-13 17:37:18 +0530

    Add face detection

--- Gesture Recognition/Context_good_features_video_pause.py
+++ Gesture Recognition/Context_good_features_video_pause.py
@@ -17,6 +17,22 @@ while True:
     ret, frame = video_capture.read()
     if ret:
 		grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		faces1 = faceCascade1.detectMultiScale(gray, 1.1, 5)
+        faces2 = faceCascade2.detectMultiScale(gray, 1.1, 5)
+        faces3 = faceCascade3.detectMultiScale(gray, 1.1, 5)
+        # Draw a rectangle around the faces
+        for (x, y, w, h) in faces1:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = frame[y:y+h, x:x+w]
+        for (x, y, w, h) in faces2:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = frame[y:y+h, x:x+w]
+        for (x, y, w, h) in faces3:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (127, 128, 0), 2)
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = frame[y:y+h, x:x+w]
 		corners = cv2.goodFeaturesToTrack(grey,900,0.01,10)
 		corners = np.int0(corners)
 		# print corners
