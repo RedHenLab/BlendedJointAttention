66e4b7fb478396deff1e0665b9d9ff800555442e
Soumitra <agarwalsoumitra1504@gmail.com>
2016-07-13 19:50:27 +0530

    Reinitialise code

--- Gesture Recognition/Context_good_features_video_pause.py
+++ Gesture Recognition/Context_good_features_video_pause.py
@@ -9,18 +9,6 @@ cam = cv2.VideoCapture(0)
 cam.set(3,640)
 cam.set(4,480)
 video_capture = cam
-frame_num = 0
-
-# Take first frame and find corners in it
-while True:
-	ret, old_frame = cam.read()
-	if ret:
-		old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
-		p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, maxCorners = 100, qualityLevel = 0.3, minDistance = 7, blockSize = 7 )
-		# Create a mask image for drawing purposes
-		mask = np.zeros_like(old_frame)
-		break
-
 while True:
     # Capture frame-by-frame
     ret, frame = video_capture.read()
@@ -31,8 +19,6 @@ while True:
 		for (x, y, w, h) in faces1:
 			roi_gray = gray[y:y+h, x:x+w]
 			roi_color = frame[y:y+h, x:x+w]
-			# calculate optical flow
-			p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
 			corners = cv2.goodFeaturesToTrack(roi_gray,90,0.01,10)
 			corners = np.int0(corners)
 			# print corners
