6cb0eb238429eaa02a5278529a311aa3c4a28fd3
Soumitra <agarwalsoumitra1504@gmail.com>
2016-07-13 17:15:29 +0530

    Add gray

--- Context/Context_good_features.py
+++ Context/Context_good_features.py
@@ -2,8 +2,8 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-img = cv2.imread('image.jpg', 0)
-
+img = cv2.imread('image.jpg')
+grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 corners = cv2.goodFeaturesToTrack(img,25,0.01,10)
 corners = np.int0(corners)
 print corners
