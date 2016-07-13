61eea0b8fdeba3b1950e84ac5a368c7123b3bc23
Soumitra <agarwalsoumitra1504@gmail.com>
2016-07-13 17:26:27 +0530

    Add video context without scene pause

--- Context/Context_good_features_video_pause.py
+++ Context/Context_good_features_video_pause.py
@@ -4,14 +4,26 @@ from matplotlib import pyplot as plt
 
 # Video capture via webcam
 cam = cv2.VideoCapture('../Test/test.mp4')
-grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-corners = cv2.goodFeaturesToTrack(grey,25,0.01,10)
-corners = np.int0(corners)
-# print corners
-for i in corners:
-    x,y = i.ravel()
-    cv2.circle(img,(x,y),1,(0,0,255),-1)
+cam.set(3,640)
+cam.set(4,480)
+video_capture = cam
+while True:
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+    print ret
+    if ret:
+    	grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		corners = cv2.goodFeaturesToTrack(grey,25,0.01,10)
+		corners = np.int0(corners)
+		# print corners
+		for i in corners:
+		    x,y = i.ravel()
+		    cv2.circle(frame,(x,y),1,(0,0,255),-1)
 
-cv2.imshow('image',img)
-cv2.waitKey(0)
+        # Display the resulting frame
+        cv2.imshow('Video', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+# Release video capture
+video_capture.release()
 cv2.destroyAllWindows()
