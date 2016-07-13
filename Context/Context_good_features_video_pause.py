a40eb4e01850a508566d439d586b28a6736ee869
Soumitra <agarwalsoumitra1504@gmail.com>
2016-07-13 17:28:56 +0530

    Indent correction

--- Context/Context_good_features_video_pause.py
+++ Context/Context_good_features_video_pause.py
@@ -11,18 +11,18 @@ while True:
     # Capture frame-by-frame
     ret, frame = video_capture.read()
     if ret:
-    	grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 		corners = cv2.goodFeaturesToTrack(grey,25,0.01,10)
 		corners = np.int0(corners)
 		# print corners
 		for i in corners:
-		    x,y = i.ravel()
-		    cv2.circle(frame,(x,y),1,(0,0,255),-1)
+			x,y = i.ravel()
+			cv2.circle(frame,(x,y),1,(0,0,255),-1)
 
-        # Display the resulting frame
-        cv2.imshow('Video', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+		# Display the resulting frame
+		cv2.imshow('Video', frame)
+		if cv2.waitKey(1) & 0xFF == ord('q'):
+			break
 # Release video capture
 video_capture.release()
 cv2.destroyAllWindows()
