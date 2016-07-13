328d1c3c9cce0e81a54f90405a40c534b386f5d5
Soumitra <agarwalsoumitra1504@gmail.com>
2016-07-13 19:53:05 +0530

    Add good old

--- Gesture Recognition/Optical_face_flow.py
+++ Gesture Recognition/Optical_face_flow.py
@@ -31,14 +31,13 @@ while True:
 		for (x, y, w, h) in faces1:
 			roi_gray = gray[y:y+h, x:x+w]
 			roi_color = frame[y:y+h, x:x+w]
+			
 			# calculate optical flow
 			p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, winSize  = (15,15), maxLevel = 2, criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
-			corners = cv2.goodFeaturesToTrack(roi_gray,90,0.01,10)
-			corners = np.int0(corners)
-			# print corners
-			for i in corners:
-				x1,y1 = i.ravel()
-				cv2.circle(frame,(x+x1,y+y1),3,(255,255,255),-1)
+			
+			# Select good points
+			good_new = p1[st==1]
+			good_old = p0[st==1]
 
 		# Display the resulting frame
 		cv2.imshow('Video', frame)
