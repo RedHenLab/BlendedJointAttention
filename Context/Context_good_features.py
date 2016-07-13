b087d436b845210fa98594dd4dfc481bcdda5b40
Soumitra <agarwalsoumitra1504@gmail.com>
2016-07-13 17:16:38 +0530

    Comment printing corners

--- Context/Context_good_features.py
+++ Context/Context_good_features.py
@@ -6,7 +6,7 @@ img = cv2.imread('image.jpg')
 grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 corners = cv2.goodFeaturesToTrack(grey,25,0.01,10)
 corners = np.int0(corners)
-print corners
+# print corners
 for i in corners:
     x,y = i.ravel()
     cv2.circle(img,(x,y),1,(0,0,255),-1)
