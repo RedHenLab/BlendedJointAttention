f81d724db351f1e12e2f067e33481c64cc8ea858
Soumitra <agarwalsoumitra1504@gmail.com>
2016-07-13 18:22:27 +0530

    Read good features

--- Gesture Recognition/Context_good_features_video_pause.py
+++ Gesture Recognition/Context_good_features_video_pause.py
@@ -20,12 +20,12 @@ while True:
 		    cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
 		    roi_gray = gray[y:y+h, x:x+w]
 		    roi_color = frame[y:y+h, x:x+w]
-			# corners = cv2.goodFeaturesToTrack(roi_gray,900,0.01,10)
-			# corners = np.int0(corners)
-			# # print corners
-			# for i in corners:
-			# 	x,y = i.ravel()
-			# 	cv2.circle(frame,(x,y),1,(0,0,255),-1)
+			corners = cv2.goodFeaturesToTrack(roi_gray,900,0.01,10)
+			corners = np.int0(corners)
+			# print corners
+			for i in corners:
+				x1,y1 = i.ravel()
+				cv2.circle(frame,(x+x1,y+y1),1,(0,0,255),-1)
 
 		# Display the resulting frame
 		cv2.imshow('Video', frame)
