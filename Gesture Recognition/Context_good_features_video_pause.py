4b158a02bdba53f11bee677beeb0286073833656
Soumitra <agarwalsoumitra1504@gmail.com>
2016-07-13 18:15:55 +0530

    Remove too many classifiers

--- Gesture Recognition/Context_good_features_video_pause.py
+++ Gesture Recognition/Context_good_features_video_pause.py
@@ -3,9 +3,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 # Defining cascade variables
-faceCascade1 = cv2.CascadeClassifier('../haarcascades/haarcascade_frontalface_alt2.xml')
-faceCascade2 = cv2.CascadeClassifier('../haarcascades/haarcascade_frontalface_default.xml')
-faceCascade3 = cv2.CascadeClassifier('../haarcascades/haarcascade_frontalface_alt.xml')
+faceCascade1 = cv2.CascadeClassifier('../haarcascades/haarcascade_frontalface_default.xml')
 
 # Video capture via webcam
 cam = cv2.VideoCapture('../Test/test.mp4')
@@ -18,19 +16,9 @@ while True:
     if ret:
 		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 		faces1 = faceCascade1.detectMultiScale(gray, 1.1, 5)
-		faces2 = faceCascade2.detectMultiScale(gray, 1.1, 5)
-		faces3 = faceCascade3.detectMultiScale(gray, 1.1, 5)
 		# Draw a rectangle around the faces
 		for (x, y, w, h) in faces1:
 		    cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
-		    roi_gray = gray[y:y+h, x:x+w]
-		    roi_color = frame[y:y+h, x:x+w]
-		for (x, y, w, h) in faces2:
-		    cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-		    roi_gray = gray[y:y+h, x:x+w]
-		    roi_color = frame[y:y+h, x:x+w]
-		for (x, y, w, h) in faces3:
-		    cv2.rectangle(frame, (x, y), (x+w, y+h), (127, 128, 0), 2)
 		    roi_gray = gray[y:y+h, x:x+w]
 		    roi_color = frame[y:y+h, x:x+w]
 		# corners = cv2.goodFeaturesToTrack(grey,900,0.01,10)
