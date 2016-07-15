84f12e06b15953bbdff3ba25551399939f3e32f5
Soumitra <agarwalsoumitra1504@gmail.com>
2016-07-15 16:25:03 +0530

    Add roi_gray definition:

--- Context/LK_face.py
+++ Context/LK_face.py
@@ -22,6 +22,7 @@ while ret == 0:
 old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
 faces = faceCascade.detectMultiScale(old_gray, 1.6, 5)
 
+roi_gray = []
 # Draw a rectangle around the faces
 for (x, y, w, h) in faces:
     roi_gray = gray[y:y+h, x:x+w]
