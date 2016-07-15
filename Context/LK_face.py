cceedc20d0efbcf8c6b12338025907c8a1f3f045
Soumitra <agarwalsoumitra1504@gmail.com>
2016-07-15 15:59:14 +0530

    Add lk params

--- Context/LK_face.py
+++ Context/LK_face.py
@@ -2,6 +2,9 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
+# Parameters for lucas kanade optical flow
+lk_params = dict( winSize  = (15,15), maxLevel = 2, criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+
 # Video capture via webcam
 cam = cv2.VideoCapture('../Test/test.mp4')
 cam.set(3,640)
