dc0a5f13e37782712b83dc4bef983ae91f3d34cd
Soumitra <agarwalsoumitra1504@gmail.com>
2016-07-16 00:40:21 +0530

    Python interpreter

--- Context/LK_face.py
+++ Context/LK_face.py
@@ -24,8 +24,9 @@ faces = faceCascade.detectMultiScale(old_gray, 1.6, 5)
 
 roi_gray = np.ndarray([])
 # Draw a rectangle around the faces
+while True:
 for (x, y, w, h) in faces:
-    roi_gray = gray[y:y+h, x:x+w]
+        roi_gray = gray[y:y+h, x:x+w]
 p0 = cv2.goodFeaturesToTrack(roi_gray, 90,0.01,10)
 
 frame_num = 0
@@ -34,48 +35,46 @@ frame_num = 0
 mask = np.zeros_like(old_frame)
 
 while True:
-	# Capture frame-by-frame
-    ret, frame = video_capture.read()
-    print frame_num
-    if ret:
-		frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-		faces = faceCascade.detectMultiScale(old_gray, 1.6, 5)
+        # Capture frame-by-frame
+        ret, frame = video_capture.read()
+        print frame_num
+        if ret:
+                frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                faces = faceCascade.detectMultiScale(old_gray, 1.6, 5)
 
-		# Draw a rectangle around the faces
-		for (x, y, w, h) in faces:
-			roi_gray = gray[y:y+h, x:x+w]
-			corners = cv2.goodFeaturesToTrack(roi_gray,90,0.01,10)
-			corners = np.int0(corners)
+                for (x, y, w, h) in faces:
+                        roi_gray = gray[y:y+h, x:x+w]
+                        corners = cv2.goodFeaturesToTrack(roi_gray,90,0.01,10)
+                        corners = np.int0(corners)
 	
 		# calculate optical flow
-		p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
+                p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
 
 		# Select good points
-		good_new = p1[st==1]
-		good_old = p0[st==1]
+                good_new = p1[st==1]
+                good_old = p0[st==1]
 
-		# print corners
 		# draw the tracks
-		for i,(new,old) in enumerate(zip(good_new,good_old)):
-		  # print i
-		  a,b = new.ravel()
-		  c,d = old.ravel()
-		  cv2.line(mask, (a,b),(c,d), (255,0,0), 2)
-		  cv2.circle(frame,(a,b),5,(255,0,0),-1)
-		frame = cv2.add(frame,mask)
+                for i,(new,old) in enumerate(zip(good_new,good_old)):
+                        a,b = new.ravel()
+                        c,d = old.ravel()
+                        cv2.line(mask, (a,b),(c,d), (255,0,0), 2)
+                        cv2.circle(frame,(a,b),5,(255,0,0),-1)
+                frame = cv2.add(frame,mask)
 
-		frame_num = frame_num + 1
+                frame_num = frame_num + 1
 
-		if frame_num>30:
+                if frame_num>30:
 			
-			# Now update the previous frame and previous points
-			old_gray = frame_gray.copy()
-			p0 = good_new.reshape(-1,1,2)
-			frame_num = 0
+                        # Now update the previous frame and previous points
+                        old_gray = frame_gray.copy()
+                        p0 = good_new.reshape(-1,1,2)
+                        frame_num = 0
+
 		# Display the resulting frame
-		cv2.imshow('Video', frame)
-		if cv2.waitKey(1) & 0xFF == ord('q'):
-			break
+                cv2.imshow('Video', frame)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                        break
 # Release video capture
 video_capture.release()
 cv2.destroyAllWindows()
