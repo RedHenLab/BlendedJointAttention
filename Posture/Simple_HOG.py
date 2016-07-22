bbc670a3a1f6a14fe0affdb33bc2cf4aa67dc314
Soumitra <agarwalsoumitra1504@gmail.com>
2016-07-23 01:57:15 +0530

    Add draw detections code

--- Posture/Simple_HOG.py
+++ Posture/Simple_HOG.py
@@ -1,5 +1,14 @@
 import cv2
+
+def draw_detections(img, rects, thickness = 1):
+    for x, y, w, h in rects:
+        # the HOG detector returns slightly larger rectangles than the real objects.
+        # so we slightly shrink the rectangles to get a nicer output.
+        pad_w, pad_h = int(0.15*w), int(0.05*h)
+        cv2.rectangle(img, (x+pad_w, y+pad_h), (x+w-pad_w, y+h-pad_h), (0, 255, 0), thickness)
+
 hog = cv2.HOGDescriptor()
 im = cv2.imread('test.jpg')
-h = hog.compute(im)
-print (h.shape,im.shape)
+found,w=hog.detectMultiScale(frame, winStride=(8,8), padding=(32,32), scale=1.05)
+draw_detections(frame,found)
+print h.shape
