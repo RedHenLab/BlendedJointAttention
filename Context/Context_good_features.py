9a324e2fdecdeadc5f3afd7c404e5ab2d8f7ada8
Soumitra <agarwalsoumitra1504@gmail.com>
2016-08-19 01:34:50 +0530

    Move test image to test

--- Context/Context_good_features.py
+++ Context/Context_good_features.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-img = cv2.imread('image.jpg')
+img = cv2.imread('../Test/image.jpg')
 grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 corners = cv2.goodFeaturesToTrack(grey,25,0.01,10)
 corners = np.int0(corners)
